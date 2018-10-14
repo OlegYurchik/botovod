ea206a77d614693463f5441b47495eeda450b805
Oleg Yurchik <oleg.yurchik@protonmail.com>
2018-10-14 15:52:09 +0300

    Edit dialog handler

--- botovod/dialogs.py
+++ botovod/dialogs.py
@@ -2,14 +2,14 @@ class DialogHandler:
     def __init__(self, dbdriver):
         self._dbdriver = dbdriver
 
-    def handler(self, agent, chat, message):
+    def __call__(self, agent, chat, message):
         follower = self._dbdriver.get_follower(agent, chat)
         if follower is None:
             next_step = None
             follower = self._dbdriver.add_follower(agent, chat)
         if next_step:
-            getattr(self, next_step)(follower, message)
-        self.start(self.follower, message)
-        
-    def start(self, follower, message):
+            getattr(self, next_step)(agent, follower, message)
+        self.start(agent, self.follower, message)
+
+    def start(self, agent, follower, message):
         raise NotImplementedError
