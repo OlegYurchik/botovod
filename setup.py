c7dcdbf771b1f82a2f809704ab03cd3eb1d1a737
Oleg Yurchik <oleg.yurchik@protonmail.com>
2019-07-10 18:22:53 +0300

    Increment version to 0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import join, dirname
 
 setup(
     name = "botovod",
-    version = "0.1.1",
+    version = "0.1.2",
     
     author = "Oleg Yurchik",
     author_email = "oleg.yurchik@protonmail.com",
